93d3f467065b2f6838254a8268137adc1723224e
Marko Polojarvi <marko@machian.com>
2023-06-16 12:17:21 +0200

    added favicon

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,31 @@ import svgr from "vite-plugin-svgr"
 import packageJson from "./package.json"
 import wasm from "vite-plugin-wasm"
 import topLevelAwait from "vite-plugin-top-level-await"
-import { comlink } from 'vite-plugin-comlink'
+import { comlink } from "vite-plugin-comlink"
+import { faviconsPlugin } from "@darkobits/vite-plugin-favicons"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), comlink(), wasm(), topLevelAwait()],
+  plugins: [
+    react(),
+    faviconsPlugin({
+      icons: {
+        favicons: {
+          source: "./src/assets/logo.png",
+        },
+        android: {
+          source: "./src/assets/logo.png",
+        },
+        appleStartup: {
+          source: "./src/assets/logo.png",
+        },
+      },
+    }),
+    svgr(),
+    comlink(),
+    wasm(),
+    topLevelAwait(),
+  ],
   resolve: {
     alias: {
       "@": "/src",
@@ -17,11 +37,9 @@ export default defineConfig({
     },
   },
   define: {
-    '__APP_VERSION__': JSON.stringify(packageJson.version),
+    __APP_VERSION__: JSON.stringify(packageJson.version),
   },
   worker: {
-    plugins: [
-      comlink()
-    ]
-  }
+    plugins: [comlink()],
+  },
 })
