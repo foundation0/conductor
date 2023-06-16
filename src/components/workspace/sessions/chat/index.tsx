efaa4da1f7540972d9cde0ee6acfce998205bcc6
Marko Polojarvi <marko@machian.com>
2023-06-16 18:00:16 +0200

    fixed input auto height bug

--- src/components/workspace/sessions/chat/index.tsx
+++ src/components/workspace/sessions/chat/index.tsx
@@ -43,7 +43,9 @@ export default function Chat() {
   const eInput = useRef<HTMLDivElement | null>(null)
   const eContainer = useRef<HTMLDivElement | null>(null)
   const [input_height, setInputHeight] = useState<number>(eInput?.current?.offsetHeight || 0)
-  const [container_height, setContainerHeight] = useState<number>(eContainer?.current?.offsetHeight ? eContainer?.current?.offsetHeight - 80 : 0)
+  const [container_height, setContainerHeight] = useState<number>(
+    eContainer?.current?.offsetHeight ? eContainer?.current?.offsetHeight - 80 : 0
+  )
   const [genController, setGenController] = useState<any>(undefined)
 
   const navigate = useNavigate()
@@ -54,24 +56,24 @@ export default function Chat() {
 
   // keep track of input height
   useEffect(() => {
-    const e_input = eInput.current;
-    if(!e_input) return
+    const e_input = eInput.current
+    if (!e_input) return
     const input_observer = new ResizeObserver(() => {
-      console.log("input height changed", e_input.offsetHeight)
+      // console.log("input height changed", e_input.offsetHeight)
       setInputHeight(e_input.offsetHeight)
-    });
+    })
     setInputHeight(e_input.offsetHeight)
-    input_observer.observe(e_input);
-    
-    const e_container = eContainer.current;
-    if(!e_container) return
+    input_observer.observe(e_input)
+
+    const e_container = eContainer.current
+    if (!e_container) return
     const container_observer = new ResizeObserver(() => {
-      console.log("container height changed", e_container.offsetHeight)
+      // console.log("container height changed", e_container.offsetHeight)
       setContainerHeight(e_container.offsetHeight)
-    });
+    })
     setContainerHeight(e_container.offsetHeight)
-    container_observer.observe(e_container);
-  }, [])
+    container_observer.observe(e_container)
+  }, [eInput?.current?.offsetHeight])
 
   // set module
   useEffect(() => {
@@ -150,7 +152,7 @@ export default function Chat() {
         })
       setMsgUpdateTs(new Date().getTime())
       if (m) {
-        let stream_response = ''
+        let stream_response = ""
         function onData({ data }: { data: any }) {
           if (data) {
             stream_response += data
@@ -380,64 +382,66 @@ export default function Chat() {
   }
 
   if (!session || !module) return null
-// style={{'marginBottom': '80px'}}
+  // style={{'marginBottom': '80px'}}
   return (
     <div className="flex flex-1 flex-col pt-2 relative" ref={eContainer}>
       <div className="flex flex-1">
-
-      
-      <AutoScroll showOption={false} scrollBehavior="auto" className={`flex flex-1`} >
-        {messages && messages?.length > 0 ? (
-          <div className="flex flex-grow text-xs justify-center text-zinc-500">
-            Active module: {session.settings.module.id} / {session.settings.module.variant}
-          </div>
-        ) : null}
-        <div className="Messages flex flex-1 flex-col" style={{ height }}>
+        <AutoScroll showOption={false} scrollBehavior="auto" className={`flex flex-1`}>
           {messages && messages?.length > 0 ? (
-            <ConversationTree
-              rows={messages}
-              onNewBranchClick={onNewBranchClick}
-              onBranchClick={onBranchClick}
-              participants={participants}
-              paddingBottom={input_height + 30}
-            />
-          ) : api_key ? (
-            <div className="flex h-full flex-col justify-center items-center">
-              <div className="flex text-zinc-500 font-semibold text-sm pb-2">Select AI to chat with...</div>
-              <div className="flex">
-                <select
-                  className="flex border rounded-lg p-2 bg-zinc-800 border-zinc-700 placeholder-zinc-400 text-white text-xs"
-                  defaultValue={
-                    workspace.defaults?.llm_module?.id
-                      ? `{"id": "${workspace.defaults?.llm_module?.id}", "variant": "${workspace.defaults?.llm_module?.variant}"}`
-                      : "click to select"
-                  }
-                  onChange={(data) => {
-                    handleModuleChange({
-                      value: data.target.value,
-                    })
-                  }}
-                >
-                  {generateLLMModuleOptions({ user_state })}
-                </select>
-              </div>
+            <div className="flex flex-grow text-xs justify-center text-zinc-500">
+              Active module: {session.settings.module.id} / {session.settings.module.variant}
             </div>
-          ) : (
-            <div className="flex justify-center items-center h-full flex-col gap-2">
-              <div className="text-zinc-500">
-                No API key for {module?.specs.meta.vendor?.name || module?.specs.meta.name} module
+          ) : null}
+          <div className="Messages flex flex-1 flex-col" style={{ height }}>
+            {messages && messages?.length > 0 ? (
+              <ConversationTree
+                rows={messages}
+                onNewBranchClick={onNewBranchClick}
+                onBranchClick={onBranchClick}
+                participants={participants}
+                paddingBottom={input_height + 30}
+              />
+            ) : api_key ? (
+              <div className="flex h-full flex-col justify-center items-center">
+                <div className="flex text-zinc-500 font-semibold text-sm pb-2">Select AI to chat with...</div>
+                <div className="flex">
+                  <select
+                    className="flex border rounded-lg p-2 bg-zinc-800 border-zinc-700 placeholder-zinc-400 text-white text-xs"
+                    defaultValue={
+                      workspace.defaults?.llm_module?.id
+                        ? `{"id": "${workspace.defaults?.llm_module?.id}", "variant": "${workspace.defaults?.llm_module?.variant}"}`
+                        : "click to select"
+                    }
+                    onChange={(data) => {
+                      handleModuleChange({
+                        value: data.target.value,
+                      })
+                    }}
+                  >
+                    {generateLLMModuleOptions({ user_state })}
+                  </select>
+                </div>
               </div>
-              <div className="text-sm">
-                <Link to="/conductor/settings">
-                  Setup {module?.specs.meta.vendor?.name || module?.specs.meta.name} module
-                </Link>
+            ) : (
+              <div className="flex justify-center items-center h-full flex-col gap-2">
+                <div className="text-zinc-500">
+                  No API key for {module?.specs.meta.vendor?.name || module?.specs.meta.name} module
+                </div>
+                <div className="text-sm">
+                  <Link to="/conductor/settings">
+                    Setup {module?.specs.meta.vendor?.name || module?.specs.meta.name} module
+                  </Link>
+                </div>
               </div>
-            </div>
-          )}
-        </div>
-      </AutoScroll>
+            )}
+          </div>
+        </AutoScroll>
       </div>
-      <div id="Input" ref={eInput} className={`absolute bottom-0 left-0 right-0 my-4 px-4 ${!api_key ? "opacity-25" : ""}`}>
+      <div
+        id="Input"
+        ref={eInput}
+        className={`absolute bottom-0 left-0 right-0 my-4 px-4 ${!api_key ? "opacity-25" : ""}`}
+      >
         <Input
           send={addMessage}
           session_id={session_id}
