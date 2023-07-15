d4fae429c9cac37e0a713e2b552f7bef33b7a987
Marko Polojarvi <marko@machian.com>
2023-07-15 12:39:40 +0200

    chore: improved profiling

--- src/components/experiences/onboarding/v1/tabs.tsx
+++ src/components/experiences/onboarding/v1/tabs.tsx
@@ -1,9 +1,18 @@
+import { ph } from "@/components/libraries/logging"
+import { useEffect } from "react"
+
 export default function Tabs() {
+  useEffect(() => {
+    ph().capture("experiences/onboarding/v1/tabs")
+  }, [])
   return (
     <div className="flex flex-col gap-3 text-left">
       <p className="flex text-xl font-semibold items-center">Open sessions</p>
       <p className="">Here you can find your open sessions as tabs.</p>
-      <p className="">Tabs come in handy when you are working on multiple conversations at the same time. You don't have to have multiple browser tabs open anymore because it's all built into Prompt.</p>
+      <p className="">
+        Tabs come in handy when you are working on multiple conversations at the same time. You don't have to have
+        multiple browser tabs open anymore because it's all built into Prompt.
+      </p>
     </div>
   )
 }
