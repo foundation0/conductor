b749bce893d17f1a1663d69ed43651c4bc192829
mjp0 <marko@machian.com>
2023-08-09 10:25:59 +0200

    fix: border flashing bug

--- src/components/workspace/sessions/tabs.tsx
+++ src/components/workspace/sessions/tabs.tsx
@@ -66,7 +66,7 @@ export default function Tabs({
         if (!s) return false
         return (
           <Link
-            className={`flex flex-row min-w-[71px] max-w-[200px] flex-nowrap flex-shrink border-0 border-r border-r-zinc-700/30 tab m-0 px-3 h-full text-xs font-semibold justify-start items-center tooltip tooltip-bottom transition-colors ph-no-capture ${
+            className={`flex flex-row min-w-[71px] max-w-[200px] flex-nowrap flex-shrink border-transparent border-0  tab m-0 px-3 h-full text-xs font-semibold justify-start items-center tooltip tooltip-bottom transition-colors ph-no-capture ${
               session_id === s.id
                 ? "tab-active bg-zinc-900/50 text-zinc-200"
                 : " bg-zinc-800 hover:bg-zinc-900/50 text-zinc-600 hover:text-zinc-300"
@@ -231,7 +231,7 @@ export default function Tabs({
   return (
     <>
       <div
-        className="h-full flex items-center justify-start p-4 leading-none cursor-pointer text-zinc-500 hover:text-zinc-200 hover:bg-zinc-900/50 border-r border-r-zinc-700/30 tooltip tooltip-right"
+        className="h-full flex items-center justify-start p-4 leading-none cursor-pointer text-zinc-500 hover:text-zinc-200 hover:bg-zinc-900/50 border-r border-r-zinc-700/30 tooltip tooltip-right transition-all"
         data-tip="Create new session"
         onClick={() => createNewSession()}
       >
@@ -244,7 +244,7 @@ export default function Tabs({
         <div className=" flex flex-row flex-1 gap-3 pr-3 justify-end items-center ">
           <div className="tooltip tooltip-left" data-tip="Show/hide notepad">
             <BiNotepad
-              className={` hover:text-zinc-200 cursor-pointer ${
+              className={` hover:text-zinc-200 cursor-pointer transition-all ${
                 item_added_to_notepad ? "animate-pulse text-zinc-200" : "text-zinc-500"
               }`}
               onClick={() => setShowNotepad()}
