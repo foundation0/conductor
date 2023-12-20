113679304a31c3105f280cbaa03f31d299d9e82c
mjp0 <marko@machian.com>
2023-12-20 11:35:55 +0700

    feat: reloads the app after inactivity to minimize sync issues

--- src/components/conductor/index.tsx
+++ src/components/conductor/index.tsx
@@ -183,6 +183,22 @@ export default function Conductor() {
         return ai
       })
     })
+
+    // TEMPORARY HACK TO MINIMIZE THE CHANGE OF SYNC FAILING
+    let timeout: any = null
+    function resetTimeout() {
+      clearTimeout(timeout)
+      timeout = setTimeout(
+        () => {
+          window?.location?.reload()
+          console.log("ref")
+        },
+        15 * 60 * 1000,
+      ) // 15 minutes
+    }
+    document.addEventListener("click", resetTimeout)
+    document.addEventListener("mousemove", resetTimeout)
+    document.addEventListener("keypress", resetTimeout)
   }, [])
 
   useEffect(() => {
